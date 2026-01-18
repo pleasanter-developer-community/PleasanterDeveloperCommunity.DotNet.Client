dd852fc07b77e95da0deaf4cb15170e05dcc85fd
Katsuyoshi Ohtsuka <ootsuka-k@vehiclevision.jp>
2026-01-18 22:43:19 +0900

    レコードの削除を追加

---
+++ PleasanterDeveloperCommunity.DotNet.Client/Models/Responses/Items/DeleteRecordResponse.cs
@@ -0,0 +1,8 @@
+namespace PleasanterDeveloperCommunity.DotNet.Client.Models.Responses.Items;
+
+/// <summary>
+/// レコード削除レスポンス
+/// </summary>
+public class DeleteRecordResponse : RecordOperationResponseBase
+{
+}
