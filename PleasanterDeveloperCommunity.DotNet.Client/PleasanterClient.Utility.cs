db64228cdc837c1b184e048e329cc3c603f18c8b
Katsuyoshi Ohtsuka <ootsuka-k@vehiclevision.jp>
2026-02-01 14:57:21 +0900

    style: if文のワンライナーを中括弧付きに修正 (Mails, Utility)

--- PleasanterDeveloperCommunity.DotNet.Client/PleasanterClient.Utility.cs
+++ PleasanterDeveloperCommunity.DotNet.Client/PleasanterClient.Utility.cs
@@ -25,7 +25,10 @@ public partial class PleasanterClient
         TimeSpan? timeout = null,
         CancellationToken cancellationToken = default)
     {
-        if (request == null) throw new ArgumentNullException(nameof(request));
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
         SetApiCredentials(request);
         return await SendRequestAsync<GetLicenseInfoResponse>(
             "/api/utility/getlicenseinfo", request, timeout, cancellationToken);
